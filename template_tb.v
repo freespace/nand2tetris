fad21f4cb4297c2240c0114d091bef5546016a3d
Shuning Bian <freespace@gmail.com>
2020-04-12 16:02:26 +1000

    Updates template_tb.v with better documentation

--- template_tb.v
+++ template_tb.v
@@ -2,15 +2,15 @@
 `define DUMPSTR(x) `"x.vcd`"
 
 module TEMPLATE_tb;
-  reg[#:0] a;
-  reg[#:0] b;
+  reg[#WIDTH#:0] a;
+  reg[#WIDTH#:0] b;
   // ...
-  wire[#:0] out;
-  reg[#:0] out_expected;
+  wire[#WIDTH#:0] out;
+  reg[#WIDTH#:0] out_expected;
 
   TEMPLATE UUT (...);
 
-  reg [#:0] testdata[0:#];
+  reg [#WIDTH#:0] testdata[0:#NROWS#];
   integer idx;
 
   initial begin
@@ -18,10 +18,12 @@ module TEMPLATE_tb;
     $dumpvars(0, TEMPLATE_tb);
 
     $readmemb("TEMPLATE_tb.data", testdata);
-    for (idx = 0; idx < #; idx = idx + 1) begin
+    for (idx = 0; idx < #NROWS#; idx = idx + 1) begin
       {..., out_expected} = testdata[idx];
       #10
-      if (out != out_expected || out === #'bx ) begin
+      // the second part of the conditional is required to detect
+      // when out is undefined, a condition not caught by !=
+      if (out != out_expected || out === #WIDTH#'bx ) begin
         $display("FAILED for test data #%d: %b", idx, testdata[idx]);
       end
     end
